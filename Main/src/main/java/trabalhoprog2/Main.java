a5c14ae2dd55614f7a25a6e93164fe367498e697
Raphael Figueiredo Secchin <raphael.secchin@edu.ufes.br>
2025-03-03 15:24:42 -0300

    testando

--- Main/src/main/java/trabalhoprog2/Main.java
+++ Main/src/main/java/trabalhoprog2/Main.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package trabalhoprog2;
 
-/**
- *
- * @author raphael
- */
 public class Main {
 
     public static void main(String[] args) {
